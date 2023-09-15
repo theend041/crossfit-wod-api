ce139ff430bf37bec5a28aef2468d7269974faa2
Samir <samir5451798@gmail.com>
2023-09-15 23:02:59 +0400

    2nd commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require("express"); 
+const bodyParser = require("body-parser");
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express(); 
+
 const PORT = process.env.PORT || 3000; 
 
-// For testing purposes 
-app.get("/", (req, res) => { 
-    res.send("<h2>It's Working!</h2>"); 
-}); 
+app.use(bodyParser.json());
+app.use("/api/v1/workouts", v1WorkoutRouter);
+
+
 
 app.listen(PORT, () => { 
     console.log(`API is listening on port ${PORT}`); 
